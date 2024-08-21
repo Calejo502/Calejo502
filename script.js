f0231c6231361784c6174b822133c9cbfb927f03
Calejo502 <calejo2011@gmail.com>
2024-08-21 17:57:36 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function displayDateTime() {
+    const now = new Date();
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+	second: 'numeric',
+        hour12: true
+    };
+    const formattedDateTime = now.toLocaleString('es-ES', options);
+    document.getElementById('datetime').textContent = formattedDateTime;
+}
+
+// Llamar a la función inmediatamente para mostrar la fecha y hora al cargar la página
+displayDateTime();
+
+// Actualizar la fecha y hora cada segundo
+setInterval(displayDateTime, 1000);
